7074b2db14956e74ea22f7ed3e5299b8dd9d3752
Herman Langner <hermandlangner@gmail.com>
2018-11-06 13:18:29 +0200

    some work on text

---
+++ Offerzen Make/Assets/Scripts/WeatherController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Application;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class WeatherController : MonoBehaviour {
+    private const string API_KEY = "88169749096b61e3b85398905927f53c";
+
+    public string CityId;
+
+    // Use this for initialization
+    void Start () {
+        var result = GetWeather(CheckSnowStatus);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void CheckSnowStatus(WeatherInfo weatherObj)
+    {
+        bool snowing = weatherObj.weather[0].main.Equals("Snow");
+    }
+    IEnumerator GetWeather(Action<WeatherInfo> onSuccess)
+    {
+        using (UnityWebRequest req = UnityWebRequest.Get(String.Format("http://api.openweathermap.org/data/2.5/weather?id={0}&APPID={1}", CityId, API_KEY)))
+        {
+            yield return req.SendWebRequest();
+            while (!req.isDone)
+                yield return null;
+            byte[] result = req.downloadHandler.data;
+            string weatherJSON = System.Text.Encoding.Default.GetString(result);
+            WeatherInfo info = JsonUtility.FromJson<WeatherInfo>(weatherJSON);
+            onSuccess(info);
+        }
+    }
+}
---
+++ Offerzen Make/Assets/WeatherInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Application
+{
+    //[Serializable]
+    public class Weather
+    {
+        public int id;
+        public string main;
+    }
+    //[Serializable]
+    public class WeatherInfo
+    {
+        public int id;
+        public string name;
+        public List<Application.Weather> weather;
+    }
+}
